f0792300360f490bc045d109a2471af9e8d9e666
SergeyPutilov <sergey98putilov@gmail.com>
2023-02-08 20:03:49 -0500

    last trasnactiones view added

--- ExpenseTracker/Controllers/DashboardController.cs
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -99,6 +99,14 @@ namespace ExpenseTracker.Controllers
                                           expense = expense == null ? 0 : expense.expense,
                                       };
 
+            // Recent Transactiones
+            ViewBag.RecentTransactiones =  await _context.Transactions
+                .Include(i => i.Category)
+                .OrderByDescending(j => j.Date)
+                .Take(5)
+                .ToListAsync();
+
+
             return View();
         }
     }
